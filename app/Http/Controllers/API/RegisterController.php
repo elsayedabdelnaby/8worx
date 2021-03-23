39ce5e6240665d37ab6d537daeb2ef54490d8b5c
elaraby <elsayed.fcis.1@gmail.com>
2021-03-23 22:57:20 +0200

    handle register function and create a register request to handle validations

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController;
+use App\Http\Requests\API\RegisterRequest;
+use App\Models\User;
+use Validator;
 use Illuminate\Http\Request;
 
 class RegisterController extends BaseController
 {
-    //
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(RegisterRequest $request)
+    {
+        $input['name'] = $request->get('name');
+        $input['email'] = $request->get('email');
+        $input['password'] = $request->get('password');
+
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
 }
